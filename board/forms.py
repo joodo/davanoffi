13fc857e6a9bd8f404aef92e91b24959765551ba
梁瀚中 <lianghanzhong@cloudist.cc>
2015-10-14 13:46:23 +0800

    解决换行问题

--- board/forms.py
+++ board/forms.py
@@ -8,10 +8,10 @@ from .models import Post, Tag
 
 
 class PostForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': '你的故事。'}))
-    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': '描述和 Tags'}))
-    music = forms.FileField(widget=forms.FileInput(attrs={'accept': 'audio/mpeg'}))
-    image = forms.FileField(widget=forms.FileInput(attrs={'accept': 'image/*'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': '你的故事。'}), required=False)
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': '描述和 Tags'}), required=False)
+    music = forms.FileField(widget=forms.FileInput(attrs={'accept': 'audio/mpeg'}), required=False)
+    image = forms.FileField(widget=forms.FileInput(attrs={'accept': 'image/*'}), required=False)
 
     def clean_image(self):
         image = self.cleaned_data.get('image', False)
@@ -29,6 +29,7 @@ class TagForm(forms.ModelForm):
     background_color = forms.CharField(widget=forms.TextInput(attrs={'type': 'color'}))
     content_text_color = forms.CharField(widget=forms.TextInput(attrs={'type': 'color'}))
     title_text_color = forms.CharField(widget=forms.TextInput(attrs={'type': 'color'}))
+
     class Meta:
         model = Tag
         fields = ['background_color', 'content_text_color', 'title_text_color',]
