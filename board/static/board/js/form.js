6887f6368b1fc934682877e9fc03a84b0a5c6fc9
梁瀚中 <lianghanzhong@cloudist.cc>
2015-10-16 18:16:58 +0800

    完成表单样式

--- board/static/board/js/form.js
+++ board/static/board/js/form.js
@@ -1,21 +1,37 @@
 $(document).ready(function() {
-    $('#id_music').change(file_field_changed);
-    $('#id_image').change(file_field_changed);
+    $('#id_music').change(fileFieldChanged);
+    $('#id_image').change(fileFieldChanged);
+    $(".clear_button").click(clearField);
 });
 
-function file_field_changed() {
+function fileFieldChanged() {
     var txt = this.value;
     var field = $(this).parent();
     var name = field.children(".file_name");
 
-    if (name.text() == "") {
+    if (txt == "") {
         field.animate({
-            width: "300px",
-            color: "white",
+            width: "40px",
+            color: "#808080",
         }, "normal", function () {
             name.text(txt);
         });
     } else {
-        name.text(txt);
+        if (name.text() == "") {
+            field.animate({
+                width: "400px",
+                color: "white",
+            }, "normal", function () {
+                name.text(txt);
+            });
+        } else {
+            name.text(txt);
+        }
     }
+}
+
+function clearField() {
+    input = $(this).parent().children("input");
+    input.val("");
+    input.change();
 }
