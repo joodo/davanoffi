6e9a725ecfb6f68bad1c7d15dfdc17bd17835789
Davanoffi Liang <wyattliang@gmail.com>
2015-10-24 13:41:30 +0900

    整理 detail li.post 的 mouse 事件

--- board/static/board/js/detail.js
+++ board/static/board/js/detail.js
@@ -1,3 +1,8 @@
+$(document).ready(function () {
+    $("li.post").mouseover(postMouseOver);
+    $("li.post").mouseleave(postMouseLeave);
+});
+
 function buttonClicked(e) {
     if (e.innerHTML == "回应…") {
         e.innerHTML = "好了。"
