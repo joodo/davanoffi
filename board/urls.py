ead796cc15500410421875d852c797e151c47fdf
Davanoffi Liang <wyattliang@gmail.com>
2015-10-25 20:32:42 +0900

    修正 detail 回应时的 bug

--- board/urls.py
+++ board/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^tag/(?P<pk>.+?)/setting/$', views.TagSettingView.as_view(), name='tag_setting'),
     url(r'^tag/(?P<tag>.+?)/$', views.PostInTag.as_view(), name='tag'),
     url(r'^(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/(?P<tag>.+?)/$', views.PostDetailView.as_view(), name='detail_with_tag'),
     url(r'^new/$', views.PostCreateView.as_view(), name='new'),
 )
